07015b701811331acf7a4bc65a45087ba7dcc570
mortezadadgar <mortezadadgar97@gmail.com>
2024-05-30 11:50:28 +0330

    Applied alwayscenter
    2024-05-10

--- dwl.c
+++ dwl.c
@@ -475,6 +475,10 @@ applyrules(Client *c)
 			}
 		}
 	}
+	if (mon) {
+		c->geom.x = (mon->w.width - c->geom.width) / 2 + mon->m.x;
+		c->geom.y = (mon->w.height - c->geom.height) / 2 + mon->m.y;
+	}
 	setmon(c, mon, newtags);
 }
 
