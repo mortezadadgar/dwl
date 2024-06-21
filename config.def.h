5682ee0ff413bbc691374fd30711f4be3c17d3a0
mortezadadgar <mortezadadgar97@gmail.com>
2024-06-21 20:16:57 +0330

    Change wmenu to emenu

--- config.def.h
+++ config.def.h
@@ -141,7 +141,7 @@ static const enum libinput_config_tap_button_map button_map = LIBINPUT_CONFIG_TA
 
 /* commands */
 static const char *termcmd[] = { "foot", NULL };
-static const char *menucmd[] = { "wmenu-run", NULL };
+static const char *menucmd[] = { "emenu_run", NULL };
 
 static const Key keys[] = {
 	/* Note that Shift changes certain key codes: c -> C, 2 -> at, etc. */
