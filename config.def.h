07443fb7e818ba9a134e3da59f35ad8cfca3b87a
mortezadadgar <mortezadadgar97@gmail.com>
2025-04-27 13:44:26 +0330

    Applied ipc
    2024-07-29

--- config.def.h
+++ config.def.h
@@ -135,6 +135,7 @@ static const Key keys[] = {
 	{ MODKEY,                    XKB_KEY_p,          spawn,          {.v = menucmd} },
 	{ MODKEY|WLR_MODIFIER_SHIFT, XKB_KEY_Return,     spawn,          {.v = termcmd} },
 	{ MODKEY,                    XKB_KEY_u,          togglescratch,  {.v = scratchpadcmd } },
+	{ MODKEY,                    XKB_KEY_b,          togglebar,      {0} },
 	{ MODKEY,                    XKB_KEY_j,          focusstack,     {.i = +1} },
 	{ MODKEY,                    XKB_KEY_k,          focusstack,     {.i = -1} },
 	{ MODKEY,                    XKB_KEY_i,          incnmaster,     {.i = +1} },
